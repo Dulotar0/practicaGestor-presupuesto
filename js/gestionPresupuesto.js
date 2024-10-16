a7c4c6cb9988ff9fa3499807ced23d8fa5797c0a
Eric Albert Alonso <ericalbertalonso@gmail.com>
2024-10-16 12:47:00 +0200

    Fundamentos de JavaScriptI

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -1,29 +1,40 @@
-// TODO: Crear las funciones, objetos y variables indicadas en el enunciado
-
-// TODO: Variable global
 var presupuesto = 0;
 
-function actualizarPresupuesto(n1) {
-    if(n1 < 0){
-
-        return -1;
-    }
-    else{
-        presupuesto = presupuesto + n1;
+function actualizarPresupuesto(nuevoPresupuesto) {
+    if(nuevoPresupuesto >= 0 && typeof nuevoPresupuesto == 'number'){
+        presupuesto = nuevoPresupuesto;
         return presupuesto;
     }
-    // TODO
+    else{
+        return -1;
+    }
 }
 
 function mostrarPresupuesto() {
-    console.log("Tu presupuesto es de ${presupuesto} €")
-    // TODO
+    return `Tu presupuesto actual es de ${presupuesto} €`;
 }
 
-function CrearGasto() {
-    // TODO
+function CrearGasto(descripcion, valor) {
+    if(valor < 0 || typeof valor != 'number'){
+        this.valor = 0;
+    }
+    else{
+        this.valor = valor;
+    }
+    this.descripcion = descripcion;
+    this.mostrarGasto = function() {
+        console.log(`Gasto correspondiente a ${descripcion} con valor ${valor} €`);
+        return`Gasto correspondiente a ${descripcion} con valor ${valor} €`;
+    }
+    this.actualizarDescripcion = function(nuevaDescripcion){
+        this.descripcion = nuevaDescripcion;
+    }
+    this.actualizarValor =function(nuevoValor){
+        if(nuevoValor > 0 && typeof nuevoValor == 'number'){
+            this.valor = nuevoValor;
+        }
+    }
 }
-
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
